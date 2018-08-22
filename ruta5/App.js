dd48b1d2b599b08d442473f540b9c343545b63ff
sborjas <iborjas@unitec.edu>
2018-08-22 01:03:12 -0600

    quite todo, nada sirve, a empezar de nuevo

--- ruta5/App.js
+++ ruta5/App.js
@@ -1,35 +1,23 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-import {DrawerNavigator} from 'react-navigation';
-import HomeScreen from './HomeScreen'
-import SettingsScreen from './SettingsScreen'
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>HOLA MUNDO!</Text>
+        <Text>Open up App.js to start working on your app!</Text>
+        <Text>Changes you make will automatically reload.</Text>
+        <Text>Shake your phone to open the developer menu.</Text>
       </View>
     );
   }
 }
 
-
-const Menu = DrawerNavigator({
-
-  Home:{
-    screen: HomeScreen
-  },
-  Settings: {
-    screen: SettingsScreen
-  }
-  
-})
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
-  }
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
 });
