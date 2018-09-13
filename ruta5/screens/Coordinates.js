aef92b865d911e93d98e3e3d43b291c1a8181b6f
sborjas <iborjas@unitec.edu>
2018-09-13 00:46:22 -0600

    mapa,agregado

---
+++ ruta5/screens/Coordinates.js
@@ -0,0 +1,58 @@
+export default [
+    {
+      key: "r_0",
+      title: "Sabor a Honduras",
+      coordinate: {
+        latitude: 30.2396,
+        longitude: -97.719925
+      }
+    },
+    {
+      key: "r_1",
+      title: "La GuacaMaya",
+      coordinate: {
+        latitude: 63.090612,
+        longitude: 21.681834
+      }
+    },
+    {
+      key: "r_2",
+      title: "Lempira Restaurant",
+      coordinate: {
+        latitude: 35.214174,
+        longitude: -80.769273
+      }
+    },
+    {
+      key: "r_3",
+      title: "Mi Bella Honduras",
+      coordinate: {
+        latitude: 29.803931,
+        longitude: -95.483306
+      }
+    },
+    {
+      key: "r_4",
+      title: "El Catrachito Restaurant",
+      coordinate: {
+        latitude: 29.707691,
+        longitude: -95.519841
+      }
+    },
+    {
+      key: "r_5",
+      title: "El Sazon Ktracho",
+      coordinate: {
+        latitude: 34.007628,
+        longitude: -118.282922
+      }
+    },
+    {
+      key: "r_6",
+      title: "Bar Honduras",
+      coordinate: {
+        latitude: 40.395485,
+        longitude: -3.662525
+      }
+    }
+  ];
--- ruta5/screens/mapa.js
+++ ruta5/screens/mapa.js
@@ -2,10 +2,18 @@ import React from 'react';
 import { StyleSheet, Text, WebView, ScrollView, Dimensions } from 'react-native';
 import {Container, Button,Body,Title,Right,Header,Left} from 'native-base';
 import Icon from 'react-native-vector-icons/Ionicons';
+import coordinates from "./Coordinates";
+import { MapView } from "expo";
 
 
 
 export default class mapa extends React.Component {
+    constructor(props) {
+        super(props);
+        // this.state = {
+        //   markers: coordinates
+        // };
+      }
     render(){
         return(
             <Container>
@@ -20,6 +28,26 @@ export default class mapa extends React.Component {
                     <Title>Restaurantes Hondureños</Title>
                 </Body>
                 </Header>
+                <Title style={{color:'black'}}>Ubicaciones de nuestras comidas Tipicas</Title>
+                <MapView
+                    style={styles.map_container}
+                    initialRegion={{
+                    latitude: 15.478512521637937,
+                    longitude: -88.03504586219789,
+                    latitudeDelta: 60,
+                    longitudeDelta: 80
+                    }}
+                >
+                    {coordinates.map(restaurant => (
+                    <MapView.Marker
+                        key={restaurant.key}
+                        title={restaurant.title}
+                        coordinate={restaurant.coordinate}
+                    />
+                    ))}
+                </MapView>
+
+
                 
 
             </Container>      
@@ -33,11 +61,12 @@ export default class mapa extends React.Component {
 const styles = StyleSheet.create({
     container: {
       flex: 1,
-      
-      alignItems: 'center',
-      justifyContent: 'center',
     },
     headerStyle: {
         backgroundColor: '#082EAF'
-    }
+    },
+    map_container: {
+        alignSelf: "stretch",
+        flex: 1
+      }
   });
